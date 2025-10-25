0b3a5232c92027a416f9138ec99787403efa4af9
david-ac1 <d.achibiri@alustudent.com>
2025-10-25 12:00:35 +0000

    updates

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,36 +1,42 @@
-#!/usr/bin/python3
-"""Script to get todos for a user from API"""
+#!/usr/bin/python3
+"""
+Script that uses a REST API to return information about
+an employee's TODO list progress.
+"""
 
 import requests
 import sys
 
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print(f"Usage: {sys.argv[0]} <employee_id>")
+        sys.exit(1)
 
-def main():
-    """main function"""
-    user_id = int(sys.argv[1])
-    todo_url = 'https://jsonplaceholder.typicode.com/todos'
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+    employee_id = int(sys.argv[1])
+    base_url = "https://jsonplaceholder.typicode.com"
 
-    response = requests.get(todo_url)
+    # Fetch user info
+    user_response = requests.get(f"{base_url}/users/{employee_id}")
+    if user_response.status_code != 200:
+        sys.exit(1)
+    user_data = user_response.json()
+    employee_name = user_data.get("name")
 
-    total_questions = 0
-    completed = []
-    for todo in response.json():
+    # Fetch todos
+    todos_response = requests.get(f"{base_url}/todos?userId={employee_id}")
+    if todos_response.status_code != 200:
+        sys.exit(1)
+    todos_data = todos_response.json()
 
-        if todo['userId'] == user_id:
-            total_questions += 1
+    # Filter completed tasks (exact boolean check)
+    completed_tasks = [task for task in todos_data if task.get("completed") is True]
 
-            if todo['completed']:
-                completed.append(todo['title'])
+    total_tasks = len(todos_data)
+    done_tasks = len(completed_tasks)
 
-    user_name = requests.get(user_url).json()['name']
+    # Print header line exactly
+    print(f"Employee {employee_name} is done with tasks({done_tasks}/{total_tasks}):")
 
-    printer = ("Employee {} is done with tasks({}/{}):".format(user_name,
-               len(completed), total_questions))
-    print(printer)
-    for q in completed:
-        print("\t {}".format(q))
-
-
-if __name__ == '__main__':
-    main()
+    # Print completed tasks exactly as required (\t + space before title)
+    for task in completed_tasks:
+        print("\t {}".format(task.get("title")))
